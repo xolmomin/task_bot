6c088ab312ed4d54442e8e23c5d03777029fb0af
kholmumin <xolmomin@gmail.com>
2020-10-26 23:30:05 +0500

    remove whitenoise from INSTALLED_APP

--- task_bot/settings.py
+++ task_bot/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'app'
 ]
